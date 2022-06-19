8863f65a96919a07a3eb5a52e33fa339548673f1
Meet Patel <meetaiims@yahoo.com>
2022-06-19 01:33:38 -0500

    learn partition, order by, rows, range in over clause

--- aggregation/functional_queries/over().sql
+++ aggregation/functional_queries/over().sql
@@ -1,5 +1,29 @@
-select *,
-convert(numeric(10, 7), NumberAttendance) / sum(NumberAttendance) over() * 100 percentAttendance,
-sum(NumberAttendance) over() total_attendance
- from tblAttendance
+
+with
+    tblAttendanceTwice
+    as
+    (
+                    select *
+            from tblAttendance
+        union all
+            select *
+            from tblAttendance
+    )
+select a.EmployeeNumber, a.AttendanceMonth, a.NumberAttendance,
+    sum(NumberAttendance) 
+over (
+    partition by a.EmployeeNumber, year(AttendanceMonth) 
+    order by AttendanceMonth
+    rows between current row and unbounded following) RowTotal,
+    sum(NumberAttendance) 
+over (
+    partition by a.EmployeeNumber, year(AttendanceMonth) 
+    order by AttendanceMonth
+    range between current row and unbounded following) RangeTotal
+--, convert(numeric(10, 7), NumberAttendance) / sum(NumberAttendance) over (partition by EmployeeNumber, year(AttendanceMonth)) * 100 percentAttendance
+from tblEmployee e join tblAttendanceTwice a
+    on e.EmployeeNumber = a.EmployeeNumber
+order by a.EmployeeNumber, AttendanceMonth
+
+
  
